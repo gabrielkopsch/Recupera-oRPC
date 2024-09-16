0ce744f938e3da99b14c3f29218fc67108388df6
gabrielkopsch <161628266+gabrielkopsch@users.noreply.github.com>
2024-09-16 09:06:39 -0300

    Script pronto

---
+++ RecuperaçaoRPC/Assets/Scripts/Apple.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEditor.Experimental.GraphView;
+
+public class Apple : MonoBehaviourPun
+{
+    const int speed = 5;
+    [SerializeField] int score;
+    Rigidbody2D rigidbody2D;
+
+    public int Score { get => score; }
+
+   
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+   
+    void Update()
+    {
+        rigidbody2D.velocity = Vector2.down * speed;
+
+        // if (transform.position.y <// -GameManager.instace.ScreenBounds.y// )
+        {
+            photonView.RPC("DestroyRPC",RpcTarget.All);
+        }
+    }
+    [PunRPC]
+    void DestroyRPC()
+    {
+        Destroy(gameObject);
+    }
+
+  
+
+    
+
+}
